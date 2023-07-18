fc8102560170361dd54fad6875042b4b61a0fc10
Nathan John <nafanjohn@gmail.com>
2023-07-18 11:54:25 +0100

    Added checkPass - 2 methods

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -1,6 +1,7 @@
 const User = require("./model");
 
 const registerUser = async (req, res) => {
+    console.log("Next called and inside controller")
     try {
         const newUser = await User.create({
             username: req.body.username,
@@ -17,12 +18,52 @@ const registerUser = async (req, res) => {
     }
 };
 
+//First login method
+// const login = async (req, res) => {
+//     try {
+//         const loggedInUser =  await User.findOne({
+//             where: {
+//                 username: req.body.username
+//             }
+//         })
+//             res.status(200).json({
+//                 message: "Logged in successfully.",
+//                 loggedInUser: loggedInUser,
+//                 user: {
+//                     username: req.body.username,
+//                     email: req.body.email
+//                 }
+//             })
+//     } catch (error) {
+//         res.status(501).json({errorMessage: error.message, error: error});
+//         console.log(error);
+//     }
+// }
+
+//Second login method using a token 
+const login = async (req, res) => {
+    try {
+        const token = await jwt.sign({id: req.user.id}, process.env.SECRET);
+        res.status(200).json({
+            message: "Success.",
+            user: {
+                username: req.user.username,
+                email: req.user.email,
+                token:token
+            }
+        })
+    } catch (error) {
+        res.status(501).json({errorMessage: error.message, error: error});
+        console.log(error);
+    }
+}
+
 const getUsers = async (req, res) => {
     try {
         const users = await User.findAll({})
-        for (let user of users){
-            user.password="";
-        }
+        // for (let user of users){
+        //     user.password="";
+        // }
         res.status(201).json({
             message: "Found all users.",
             users:users
@@ -35,5 +76,6 @@ const getUsers = async (req, res) => {
 
 module.exports = {
     registerUser,
-    getUsers
+    getUsers,
+    login
 }
